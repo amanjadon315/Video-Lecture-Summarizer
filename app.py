c18c0f44299032bcf4a30b9f320ec222db05f49e
Aman Jadon <114644868+amanjadon315@users.noreply.github.com>
2026-03-29 15:06:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -189,17 +189,7 @@ def extract_audio_from_url(video_url):
             },
         }
         
-        """ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-            'outtmpl': output_path.replace('.mp3', ''),
-            'quiet': True,
-            'no_warnings': True,
-        }""""
+        
         
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=True)
